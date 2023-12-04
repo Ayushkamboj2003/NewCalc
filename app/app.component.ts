f6a012162cac1bfffcfb246f6eec4a1dbc10186e
Ayushkamboj2003 <149534391+Ayushkamboj2003@users.noreply.github.com>
2023-12-04 17:16:48 +0100

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,65 +7,70 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'NewCalc';
-  toshow = ''
-  currentvalue = ''
-  valid: boolean = true
-  typecalc:string = 'scientifica'
-  sqaureroot :  number =25;
+  toshow = '';
+  currentvalue: any = '';
+  valid: boolean = true;
+  typecalc: string = 'scientifica';
+  sqaureroot: number = 25;
 
-ngOninit(){
-  if(this.valid==true){
-    this.typecalc='Normale'
+  ngOninit() {
+    if (this.valid == true) {
+      this.typecalc = 'Normale';
+    }
   }
-}
-change(){
-  this.valid =!this.valid
-  if (this.valid == false){
-this.typecalc= 'Normale'
+  change() {
+    this.valid = !this.valid;
+    if (this.valid == false) {
+      this.typecalc = 'Normale';
+    } else {
+      this.typecalc = 'Scientifica';
+    }
   }
-  else{
-this.typecalc= 'Scientifica'
+
+  write(value: any) {
+    if (this.currentvalue == '0') {
+      this.currentvalue = value;
+    }
+    if (this.currentvalue != '0') {
+      this.currentvalue = this.currentvalue + value;
+    }
+    this.toshow = this.currentvalue;
+  }
+  equals() {
+    this.toshow = eval(this.currentvalue);
+    this.currentvalue = this.toshow;
+  }
+  clear() {
+    this.currentvalue = '';
+    this.toshow = '0';
+  }
+  back() {
+    this.currentvalue = this.currentvalue.slice(0, -1);
+    this.toshow = this.currentvalue;
+    if (this.toshow == '') {
+      this.toshow = '0';
+    }
+  }
+  pi() {
+    this.currentvalue += Math.PI;
+    this.toshow += 'π';
   }
 
-  
-}
-
-write(value:any)
-  {
-if(this.currentvalue=='0'){this.currentvalue = value}
-if (this.currentvalue !='0') 
-{this.currentvalue =this.currentvalue + value}
-this.toshow=this.currentvalue
+  squareroots() {
+    const radice: any = Math.sqrt(this.currentvalue);
+    this.toshow = radice;
+    this.currentvalue = radice;
   }
-equals()
-{
-  this.toshow = eval(this.currentvalue)
-  this.currentvalue = this.toshow
-  
-}
-clear(){
-this.currentvalue = '';
-this.toshow = '0'}
-back(){
-  this.currentvalue = this.currentvalue.slice(0, -1)
-  this.toshow = this.currentvalue
-  if(this.toshow ==''){ this.toshow ='0'}
- 
-
-
-}
-pi(value:any){
-this.currentvalue += Math.PI
-this.toshow += 'π'
-}
-squareroots(): number {
- // var squareroot = Math.sqrt(num);
-return Math.sqrt(this.sqaureroot);
-
-}
-
+  square() {
+    this.currentvalue += '** 2';
+    this.toshow += '²';
+  }
+  percentage(){
+    this.currentvalue += '/100*'
+    this.toshow +='% of'
+  }
 }
